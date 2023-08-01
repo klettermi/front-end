54f5b407bcc13378bb315d29f9100bab6f0a3c3d
YiHwanRyu <yihwandev@gmail.com>
2023-08-01 17:37:37 +0900

    feat: 로그인 페이지(index) 구현

---
+++ static/js/app/index.js
@@ -0,0 +1,35 @@
+function login() {
+    let student_num = document.getElementById("student_num").value;
+    let password = document.getElementById("password").value;
+
+    let loginUrl = "http://localhost:8080/api/auth/login";
+
+    fetch(loginUrl, {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json",
+        },
+        body: JSON.stringify({
+            "student_num": student_num,
+            "password": password,
+        }),
+    })
+    .then((response) => {
+        let token = response.headers.get("Authorization");
+        localStorage.setItem("Authorization", token);
+        return response.json();
+    })
+    .then(data => {
+        console.log(data.status_code)
+        if (data.status_code == 200) {
+            alert(data.message);
+            window.location.href = "../../../pages/course-registration.html";
+        } else {
+            alert("로그인 정보가 일치하지 않습니다.");
+        }
+    })
+    .catch(error => {
+        console.error("로그인 중 에러발생:", error);
+        alert("로그인 중 에러가 발생하였습니다. 다시 시도해 주세요.");
+    });
+}
