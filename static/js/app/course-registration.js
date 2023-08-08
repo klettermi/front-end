d6b261c6228f2484f77c48aab26823a5ffbbddd8
thing-zoo <cathy4025@gmail.com>
2023-08-08 11:48:59 +0900

    feat: 수강신청 api 추가

--- static/js/app/course-registration.js
+++ static/js/app/course-registration.js
@@ -1,12 +1,16 @@
+$(document).ready(function () {
+    getRegistration();
+});
+
 function logout() {
     localStorage.removeItem("Authorization");
     // location.replace("메인페이지 주소")
 }
 
 function search_class() {
-    const year = 2023;
+    const year = 2022;
     const semester = 1;
-    const subjectCd = $("#subject-code").val();
+    const subjectCd = $("#subject-code").text();
     const depart = $("#depart option:selected").text();
     const sort = $("#sort option:selected").text();
 
@@ -63,7 +67,7 @@ function search_class() {
                                 <td>${limitation}</td>
                                 <td>${numberOfCurrent}</td>
                                 <td>
-                                    <button type="button" class="btn btn-primary btn-sm" onclick="register(${courseId})">신청</button>
+                                    <button type="button" class="btn btn-primary btn-sm" onclick="pre_register(${courseId})">신청</button>
                                 </td>
                             </tr>
                             `
@@ -79,6 +83,104 @@ function search_class() {
         });
 }
 
-function register(courseId) {
+function pre_register(courseId) {
     console.log(courseId)
+}
+
+function register(courseId) {
+    let url = "http://localhost:8080/api/registration/" + courseId;
+    let token = localStorage.getItem("Authorization");
+    fetch(url, {
+        method: "POST",
+        headers: {
+            "Authorization": token
+        },
+    })
+    .then(response => response.json())
+    .then(result => {
+        if (result.success) {
+            alert(result.msg);
+            location.reload();
+        } else {
+            alert(result.errors);
+        }
+    });
+}
+
+function cancel(registrationId) {
+    let url = "http://localhost:8080/api/registration/" + registrationId;
+    let token = localStorage.getItem("Authorization");
+    fetch(url, {
+        method: "DELETE",
+        headers: {
+            "Authorization": token
+        },
+    })
+    .then(response => response.json())
+    .then(result => {
+        if (result.success) {
+            alert(result.msg);
+            location.reload();
+        } else {
+            alert(result.errors);
+        }
+    });
+}
+
+function getRegistration() {
+    let url = "http://localhost:8080/api/registration";
+    let token = localStorage.getItem("Authorization");
+    fetch(url, {
+        method: "GET",
+        headers: {
+            "Authorization": token
+        },
+    })
+    .then(response => response.json())
+    .then(result => {
+        console.log(result);
+        if (result.success) {
+            $('#registration-list').empty();
+
+            result.data.forEach(element => {
+                const registrationId = element.registrationId;
+                const collegeName = element.collegeName;
+                const departmentName = element.departmentName == null ? '-' : element.departmentName;
+                const majorName = element.majorName  == null ? '-' : element.majorName;
+                const sort = element.sort;
+                const subjectCode = element.subjectCode;
+                const division = element.division;
+                const subjectName = element.subjectName;
+                const credit = element.credit;
+                const professorName = element.professorName;
+                const timetable = element.timetable;
+                const limitation = element.limitation;
+                const current = element.current;
+
+                let temp = `
+                    <tr>
+                        <td>${collegeName}</td>
+                        <td>${departmentName}</td>
+                        <td>${majorName}</td>
+                        <td>${sort}</td>
+                        <td>${subjectCode}</td>
+                        <td>${division}</td>
+                        <td>${subjectName}</td>
+                        <td>${credit}</td>
+                        <td>${professorName}</td>
+                        <td>${timetable}</td>
+                        <td>${limitation}</td>
+                        <td>${current}</td>
+                        <td>
+                            <button type="button" class="btn btn-primary btn-sm" onclick="cancel(${registrationId})">취소</button>
+                        </td>
+                    </tr>
+                    `
+                $('#registration-list').append(temp)
+            })
+        } else {
+            console.log(result.errors)
+        }
+    });
+
 }
